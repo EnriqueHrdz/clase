ab52d80be4c9009092d196cb1fed2fda58dfee67
EnriqueHrdz <38929751+EnriqueHrdz@users.noreply.github.com>
2020-07-09 01:17:48 -0500

    Add files via upload

---
+++ tests/Feature/Http/Controllers/Auth/CreateControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    /** @test  */
+    public function create_products(){
+
+        $response = $this->post('new/product');
+
+        $response->assertStatus(405);
+
+    }
+}
